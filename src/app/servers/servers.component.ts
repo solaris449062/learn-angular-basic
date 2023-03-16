b80d0c2b4abdc046a1cfef97d50fbf78c68656fb
solaris449062 <spark@spark.local>
2023-03-16 13:41:11 -0600

    basic component creation nad nesting

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  template: `
+  <app-server></app-server>
+  here goes additional html if necessary
+  <app-server></app-server>`,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+
+}
