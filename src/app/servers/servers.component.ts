749f6f84b9065396a5e2994d4a8a37d6701b28b3
solaris449062 <spark@spark.local>
2023-03-17 14:34:43 -0600

    data-binding and practice

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  here goes additional html if necessary
-  <app-server></app-server>`,
+  // selector: 'app-servers',
+  // selector: '[app-servers]',
+  selector: '.app-servers',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
+
 export class ServersComponent {
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = 'No server was created!';
+  serverName: string = '';
+  userName: string = '';
+
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.allowNewServer = true;
+      }
+    , 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = `Server ${this.serverName} was created!`;
+  }
+
+  setUserName(name:string) {
+    this.userName = name;
+  }
+  // onUpdateServerName(event: any) {
+  //   // this.serverName = event.target.value; // less desirable because it would only work when it's input
+  //   this.serverName = (<HTMLInputElement>event.target).value
+  // }
+
+
 
 }
